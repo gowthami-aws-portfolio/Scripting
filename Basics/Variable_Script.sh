de92daf5d52beee27aea396e62aa576285cdeb88
Gowthami <gowthami.awsprofile@gmail.com>
2025-10-14 21:21:55 +0530

    Variable

---
+++ Basics/Variable_Script.sh
@@ -0,0 +1,18 @@
+#!/bin/bash 
+
+###############
+#Author: Gowthami
+#Date: 14 Oct 2025
+#About: Variable_Script
+
+
+###################################
+
+#Variable defining
+
+name="Gowthami"
+learning="Scripting"
+
+read -p "enter your favourite cloud platform (AWS/AZURE/GCP): " cloud
+
+echo "My name is $name, I am learning $learning, and I prefer to work on $cloud"
